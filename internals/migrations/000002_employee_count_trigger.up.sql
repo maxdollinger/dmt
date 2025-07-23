26435b19ba48d901d01d1b43786bf5853cc3850d
Maximilian Dollinger <max@dollinger.dev>
2025-07-23 07:27:57 +0200

    changed notification to only run on employee updates

--- internals/migrations/000002_employee_count_trigger.up.sql
+++ internals/migrations/000002_employee_count_trigger.up.sql
@@ -11,20 +11,18 @@ BEGIN
     FROM device
     WHERE device.employee = NEW.employee;
 
-    IF device_count >= 3 THEN
-        PERFORM pg_notify('device_count', 
-            json_build_object(
-                'employee', employee,
-                'count', device_count
-            )::text
-        );
-    END IF;
+    PERFORM pg_notify('device_count', 
+        json_build_object(
+            'employee', employee,
+            'count', device_count
+        )::text
+    );
     
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER device_count_notification_trigger
-    AFTER INSERT OR UPDATE ON device
+    AFTER INSERT OR UPDATE OF employee ON device
     FOR EACH ROW
     EXECUTE FUNCTION notify_device_count();
