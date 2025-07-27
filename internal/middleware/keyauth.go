b08c3c06350c70b973784be24e8f691380dbdcd1
Maximilian Dollinger <max@dollinger.dev>
2025-07-27 08:37:31 +0200

    logging of auth error

--- internal/middleware/keyauth.go
+++ internal/middleware/keyauth.go
@@ -3,8 +3,10 @@ package middleware
 import (
 	"crypto/subtle"
 	"encoding/base64"
+	"errors"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/log"
 	"github.com/gofiber/fiber/v2/middleware/keyauth"
 )
 
@@ -13,13 +15,18 @@ func KeyAuthMiddleware(apiKey string) fiber.Handler {
 		Validator: func(c *fiber.Ctx, key string) (bool, error) {
 			providedKey, err := base64.StdEncoding.DecodeString(key)
 			if err != nil {
-				return false, keyauth.ErrMissingOrMalformedAPIKey
+				return false, err
 			}
 
 			if subtle.ConstantTimeCompare(providedKey, []byte(apiKey)) == 1 {
 				return true, nil
 			}
-			return false, keyauth.ErrMissingOrMalformedAPIKey
+
+			return false, errors.New("invalid API key")
+		},
+		ErrorHandler: func(c *fiber.Ctx, err error) error {
+			log.Warnf("Access denied from '%s' - %s", c.IP(), err.Error())
+			return c.Status(fiber.StatusUnauthorized).Send(nil)
 		},
 	})
 }
